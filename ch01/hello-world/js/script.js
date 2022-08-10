42ddbcf3b1c386c149f6f5738e50a3720c3cacbf
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2022-08-10 23:04:14 +0300

    some change and extra examles

--- ch01/hello-world/js/script.js
+++ ch01/hello-world/js/script.js
@@ -1,5 +1,33 @@
-let h1 = React.createElement('h1', null, 'Hello world!!!')
+let h1 = React.createElement('h1', null, "Hello World!");
+
 ReactDOM.render(
   h1,
-  document.getElementById('content')
+  document.getElementById('content1')
+);
+
+ReactDOM.render(
+  React.createElement('p', null, 'Typical description.'),
+  document.getElementById('content2')
+)
+
+ReactDOM.render(
+  React.createElement('button', null, 'This is a perfect button!'),
+  document.querySelector('.button_wrapper')
+)
+
+let link = React.createElement('a',
+  {href: '#'},
+  'An empty link with "#"'
+)
+
+ReactDOM.render(
+  link,
+  document.querySelector('.link_wrapper')
+);
+
+let h2 = React.createElement('h2', null, 'The second heading');
+
+ReactDOM.render(
+  React.createElement('div', null, h2, h2, h2),
+  document.querySelector('.wrapper')
 )
