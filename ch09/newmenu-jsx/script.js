39a1aaca211a41e04b2a6dddbbd4d9f6c4f23f01
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2022-08-23 17:04:03 +0300

    chapter 10

--- ch09/newmenu-jsx/script.js
+++ ch09/newmenu-jsx/script.js
@@ -1,1 +1,32 @@
+class Menu extends React.Component {
+  render() {
+    let menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us', 'Extra information'];
 
+    return React.createElement(
+      'div',
+      null,
+      menus.map((v, i) => {
+        return React.createElement(
+          'div',
+          { key: i, className: 'menu-elem' },
+          React.createElement(Link, { label: v })
+        );
+      })
+    );
+  }
+
+};
+
+class Link extends React.Component {
+  render() {
+    const url = '/' + this.props.label.toLowerCase().trim().replace(' ', '-');
+
+    return React.createElement(
+      'a',
+      { href: url },
+      this.props.label
+    );
+  }
+};
+
+ReactDOM.render(React.createElement(Menu, null), document.getElementById('menu'));
