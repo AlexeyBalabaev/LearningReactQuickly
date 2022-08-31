11d6ed8d53ba0258f8043cba2ca10bf6afe2bfe1
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2022-08-31 16:50:02 +0300

    new edits/ chapter 13

--- ch13/my-naive-router/jsx/app.jsx
+++ ch13/my-naive-router/jsx/app.jsx
@@ -6,10 +6,10 @@ const mapping = {
   '#profile': <div>Profile (<a href="#">home</a>)</div>,
   '#accounts': <div>Accounts (<a href="#">home</a>)</div>,
   '*': <div>Dashboard:<br />
-  <a href="#profile">Profile</a>
-  <br />
-  <a href="#accounts">Accounts</a>
-  </div>
+      <a href="#profile">Profile</a>
+      <br />
+      <a href="#accounts">Accounts</a>
+    </div>
 }
 
 ReactDOM.render(
