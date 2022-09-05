ef1bac0ef43906f4400d5bbe481e14a561845f46
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2022-09-05 16:47:11 +0300

    small edits/chapter 14

--- ch14/redux-netflix/src/modules/movies.js
+++ ch14/redux-netflix/src/modules/movies.js
@@ -1,7 +1,7 @@
-const { handleActions } = require('redux-actions')
+const { handleActions } = require('redux-actions');
 
-const FETCH_MOVIES = 'movies/FETCH_MOVIES'
-const FETCH_MOVIE = 'movies/FETCH_MOVIE'
+const FETCH_MOVIES = 'movies/FETCH_MOVIES';
+const FETCH_MOVIE = 'movies/FETCH_MOVIE';
 
 module.exports = {
   fetchMoviesActionCreator: (movies) => ({
